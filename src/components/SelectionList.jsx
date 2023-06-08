631ed00f313c67fdc6a4bfb7642f218b708133ee
Alan Bustamante <alan.bustamante@relometrics.com>
2023-06-08 12:07:20 -0700

    add prettier and reformat

--- src/components/SelectionList.jsx
+++ src/components/SelectionList.jsx
@@ -1,126 +1,123 @@
-import {createContext, useContext, useEffect, useState} from "react";
-import {v4} from "uuid";
+import { createContext, useContext, useEffect, useState } from "react";
+import { v4 } from "uuid";
 
 const SelectionListContext = createContext(null);
 
 const useSelectionListContext = () => useContext(SelectionListContext);
 
-const SelectionList = ({
-                           children,
-                           isSingleSelection = false,
-                       }) => {
-    const [selectedIds, setSelectedIds] = useState([]);
-    const uuid = v4();
-    const [searchText, setSearchText] = useState("");
-    useEffect(() => setSelectedIds([]), [isSingleSelection]);
+const SelectionList = ({ children, isSingleSelection = false }) => {
+  const [selectedIds, setSelectedIds] = useState([]);
+  const uuid = v4();
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => setSelectedIds([]), [isSingleSelection]);
 
-    const handleSelectionId = (id) => {
-        // NOTE: if we need to support forced selections we can merge selections here
-        if (isSingleSelection) {
-            setSelectedIds([id]);
-            return;
+  const handleSelectionId = (id) => {
+    // NOTE: if we need to support forced selections we can merge selections here
+    if (isSingleSelection) {
+      setSelectedIds([id]);
+      return;
+    }
+
+    setSelectedIds((prev) => {
+      let hadSelection = false;
+
+      // went for reducer since might need to filter or add and saw this as the best approach
+      const newSelections = prev.reduce((acc, curr) => {
+        if (curr === id) {
+          hadSelection = true;
+          return acc;
         }
 
-        setSelectedIds((prev) => {
-            let hadSelection = false;
+        acc.push(curr);
+        return acc;
+      }, []);
+      return hadSelection ? newSelections : [...newSelections, id];
+    });
+  };
 
-            // went for reducer since might need to filter or add and saw this as the best approach
-            const newSelections = prev.reduce((acc, curr) => {
-                if (curr === id) {
-                    hadSelection = true;
-                    return acc;
-                }
+  const resetSelectedIds = () => setSelectedIds([]);
 
-                acc.push(curr);
-                return acc;
-            }, []);
-            return hadSelection ? newSelections : [...newSelections, id];
-        });
-    };
-
-    const resetSelectedIds = () => setSelectedIds([]);
-
-    return (
-        <SelectionListContext.Provider
-            value={{
-                contextId: uuid,
-                selectedIds,
-                isSingleSelection,
-                searchText,
-                setSearchText,
-                resetSelectedIds,
-                addSelection: handleSelectionId,
-            }}
-        >
-            <div className="selection-list">{children}</div>
-        </SelectionListContext.Provider>
-    );
-};
-
-const List = (props) => {
-    return <ul className="selection-list__options">{props.children}</ul>;
-};
-
-const Item = ({value, text}) => {
-    const {
-        contextId,
-        addSelection,
+  return (
+    <SelectionListContext.Provider
+      value={{
+        contextId: uuid,
         selectedIds,
         isSingleSelection,
         searchText,
-    } = useSelectionListContext();
-    const isSelected = !!selectedIds.find((selection) => selection === value);
-
-    const shouldRender = text.includes(searchText);
-
-    return shouldRender ? (
-        <li className="selection-list__option" onClick={() => addSelection(value)}>
-            <input
-                type={isSingleSelection ? "radio" : "checkbox"}
-                name={contextId}
-                value={value}
-                checked={isSelected}
-                readOnly
-            />
-            {text}
-        </li>
-    ) : null;
+        setSearchText,
+        resetSelectedIds,
+        addSelection: handleSelectionId,
+      }}
+    >
+      <div className="selection-list">{children}</div>
+    </SelectionListContext.Provider>
+  );
 };
 
-const SubmitButton = ({text, onSubmit, shouldKeepSelections}) => {
-    const {selectedIds, resetSelectedIds} = useSelectionListContext()
-    const handleSubmit = () => {
-        onSubmit(selectedIds);
-        if (!shouldKeepSelections) {
-            resetSelectedIds()
-        }
+const List = (props) => {
+  return <ul className="selection-list__options">{props.children}</ul>;
+};
+
+const Item = ({ value, text }) => {
+  const {
+    contextId,
+    addSelection,
+    selectedIds,
+    isSingleSelection,
+    searchText,
+  } = useSelectionListContext();
+  const isSelected = !!selectedIds.find((selection) => selection === value);
+
+  const shouldRender = text.includes(searchText);
+
+  return shouldRender ? (
+    <li className="selection-list__option" onClick={() => addSelection(value)}>
+      <input
+        type={isSingleSelection ? "radio" : "checkbox"}
+        name={contextId}
+        value={value}
+        checked={isSelected}
+        readOnly
+      />
+      {text}
+    </li>
+  ) : null;
+};
+
+const SubmitButton = ({ text, onSubmit, shouldKeepSelections }) => {
+  const { selectedIds, resetSelectedIds } = useSelectionListContext();
+  const handleSubmit = () => {
+    onSubmit(selectedIds);
+    if (!shouldKeepSelections) {
+      resetSelectedIds();
     }
-    return (
-        <button onClick={handleSubmit} disabled={!selectedIds?.length}>
-            {" "}
-            {text}{" "}
-        </button>
-    );
-}
+  };
+  return (
+    <button onClick={handleSubmit} disabled={!selectedIds?.length}>
+      {" "}
+      {text}{" "}
+    </button>
+  );
+};
 
 const InputSearch = () => {
-    const {setSearchText: setContextSearchText} = useSelectionListContext();
-    const [searchText, setSearchText] = useState("");
+  const { setSearchText: setContextSearchText } = useSelectionListContext();
+  const [searchText, setSearchText] = useState("");
 
-    useEffect(() => {
-        const timeout = setTimeout(() => setContextSearchText(searchText), 250);
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [searchText]);
+  useEffect(() => {
+    const timeout = setTimeout(() => setContextSearchText(searchText), 250);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [searchText]);
 
-    return (
-        <input
-            type="text"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-        />
-    );
+  return (
+    <input
+      type="text"
+      value={searchText}
+      onChange={(e) => setSearchText(e.target.value)}
+    />
+  );
 };
 
 SelectionList.List = List;
